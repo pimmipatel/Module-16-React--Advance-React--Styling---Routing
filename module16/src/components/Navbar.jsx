57d59571540d6917e4860c4089c239b679c74da6
pimmipatel <patelpimmi02112002@gmail.com>
2025-09-16 11:08:29 +0530

    Update Navbar.jsx

--- module16/src/components/Navbar.jsx
+++ module16/src/components/Navbar.jsx
@@ -1,42 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-  const linkStyle = {
-    marginRight: '15px',
-    textDecoration: 'none',
-    color: 'black',
-    fontWeight: '500',
-  };
-
-  const activeStyle = {
-    color: 'blue',
-    fontWeight: 'bold',
-  };
-
   return (
-    <nav style={{ padding: '10px', background: '#f4f4f4' }}>
-      <NavLink
-        to="/"
-        style={linkStyle}
-        end
-        className={({ isActive }) => (isActive ? 'active-link' : '')}
-      >
+    <nav>
+      <NavLink to="/" end>
         Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        style={linkStyle}
-        className={({ isActive }) => (isActive ? 'active-link' : '')}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/contact"
-        style={linkStyle}
-        className={({ isActive }) => (isActive ? 'active-link' : '')}
-      >
-        Contact
-      </NavLink>
+      </NavLink>{' '}
+      | <NavLink to="/about">About</NavLink> |{' '}
+      <NavLink to="/contact">Contact</NavLink>
     </nav>
   );
 }
